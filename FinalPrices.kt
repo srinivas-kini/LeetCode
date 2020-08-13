5aa2178e5bfebaa2b7bdcb018a2c7ef69dc7cb5f
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2020-08-13 22:54:07 +0530

    Add files via upload

---
+++ FinalPrices.kt
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+fun finalPrices(prices: IntArray): IntArray {
+    var res = IntArray(prices.size)
+
+    for (i in prices.indices) {
+        var curr = prices[i]
+        var j = i + 1
+
+        if (j == prices.size) {
+            res[i] = prices[i]
+            break
+        }
+
+        while (j < prices.size) {
+            if (prices[j] <= curr) {
+                res[i] = curr - prices[j]
+                break
+            } else {
+                res[i] = prices[i]
+                j++
+            }
+        }
+
+    }
+    return res
+}
