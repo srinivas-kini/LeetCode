add16f640a9a451e71715808f97e1f0b15384790
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2020-08-14 13:12:38 +0530

    Add files via upload

---
+++ IsPowerOfTwo.kt
@@ -0,0 +1,14 @@
+import kotlin.math.pow
+fun isPowerOfTwo(n: Int): Boolean {
+    if(n == 1)
+        return true
+
+     var exponent = 0
+     while (2.0.pow(exponent) < n)
+     {
+         exponent ++
+         if(2.0.pow(exponent) == n.toDouble())
+             return true
+     }
+    return false
+}
