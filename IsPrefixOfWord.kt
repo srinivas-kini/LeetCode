6d3f3ffac27cabaeb43561c07a651222a0742bc7
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2020-08-19 09:43:42 +0530

    Add files via upload

---
+++ IsPrefixOfWord.kt
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+fun isPrefixOfWord(sentence: String, searchWord: String): Int {
+    var words = sentence.split(" ")
+    for (i in words.indices) {
+        if (words[i].startsWith(searchWord))
+            return i + 1
+    }
+    return 0
+}
+
