ccfab038f5b953a4de42699310f53f86388cb86b
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2020-08-20 19:02:09 +0530

    Update IsAnagram.kt

--- IsAnagram.kt
+++ IsAnagram.kt
@@ -2,8 +2,7 @@
 fun isAnagram(s: String, t: String): Boolean {
     return mp(s) == mp(t)
 }
-
-private fun mp(str: String): MutableMap<Char, Int> {
+fun mp(str: String): MutableMap<Char, Int> {
     val map = mutableMapOf<Char, Int>()
     str.forEach { map[it] = map.getOrDefault(it, 0) + 1 }
     return map
