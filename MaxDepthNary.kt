42fc34e5b19a3cc68e86e72f6ebf1926a7120cba
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2020-08-29 21:17:34 +0530

    Delete MaxDepthNary.kt

--- MaxDepthNary.kt
+++
@@ -1,9 +0,0 @@
-//https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
-fun maxDepth(root: Node?): Int {
-    if (root == null) return 0
-    var max = 0
-    root.children.forEach { child ->
-        max = maxOf(max, maxDepth(child))
-    }
-    return max + 1
-}
