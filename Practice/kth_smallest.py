34a3fad378109876b62f93a190399c10bc5628ee
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2021-05-07 08:52:34 +0530

    Update kth_smallest.py

--- Practice/kth_smallest.py
+++ Practice/kth_smallest.py
@@ -1,7 +1,5 @@
+# https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/submissions/
 class Solution:
     def kthSmallest(self, matrix, k):
-        flattened = sorted(set([x for l in matrix for x in l]))
+        flattened = sorted([x for l in matrix for x in l])
         return flattened[k - 1]
-s = Solution()
-
-print(s.kthSmallest([[1,5,9],[10,11,13],[12,13,15]],8))
