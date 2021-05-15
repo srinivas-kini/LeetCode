a21a6dc819327eff5856a15dd97a7d56b60f5c04
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2021-05-15 09:00:45 +0530

    Update shifting_letters.py

--- Practice/shifting_letters.py
+++ Practice/shifting_letters.py
@@ -1,11 +1,15 @@
 # https://leetcode.com/problems/shifting-letters/
 class Solution(object):
-    letters = 'abcdefghijklmnopqrstuvwxyz'
+    letters = "abcdefghijklmnopqrstuvwxyz"
+
     def shiftingLetters(self, s, shifts):
         shifted = ""
+        _sum = sum(shifts)
         for i in range(len(s)):
-            shifted = self.shift(to_shift=shifted + s[i], by=shifts[i])
+            shifted += self.shift(to_shift=s[i], by=_sum)
+            _sum -= shifts[i]
         return shifted
 
     def shift(self, to_shift, by):
-        return "".join(self.letters[(self.letters.index(c) + by) % 26] for c in to_shift)
+        return self.letters[(self.letters.index(to_shift) + by) % 26]
+
