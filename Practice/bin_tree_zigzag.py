9c7e2465827047fd6111897279b6951da7b9c9a9
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2021-07-21 16:53:06 +0530

    Create bin_tree_zigzag.py

---
+++ Practice/bin_tree_zigzag.py
@@ -0,0 +1,19 @@
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        l2r = False
+        q = [root]
+        ans = []
+        while q:
+            vals = []
+            l2r = not l2r
+            for _ in range(len(q)):
+                currnode = q.pop(0)
+                vals.append(currnode.val)
+                if currnode.left:
+                    q.append(currnode.left)
+                if currnode.right:
+                    q.append(currnode.right)
+            ans.append(vals if l2r else list(reversed(vals)))
+        return ans
