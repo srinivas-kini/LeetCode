326cd3b68d993b5878e3ff1187cde7f411502092
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2021-09-01 16:31:48 -0400

    Update coin_change.py

--- Practice/coin_change.py
+++ Practice/coin_change.py
@@ -1,1 +1,11 @@
-
+# https://leetcode.com/problems/coin-change/submissions/
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        dp = [0] + [amount+1]*amount
+        
+        for i in range(1,len(dp)):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i-coin]+1,dp[i])              
+        return dp[-1] if dp[-1] != amount + 1 else -1
