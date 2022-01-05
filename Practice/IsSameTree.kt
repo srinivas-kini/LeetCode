f2fe52185196be31dd061912d59fb22a81ce9f18
Srinivas <68226496+srinivas-kini@users.noreply.github.com>
2022-01-05 17:32:56 -0500

    Update and rename IsSameTree.kt to IsSameTree.java

--- Practice/IsSameTree.kt
+++
@@ -1,5 +0,0 @@
-//https://leetcode.com/problems/same-tree/
-fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean =
-    if (p == null && q == null) true
-    else if (p?.`val` != q?.`val` || p == null && q != null || p != null && q == null) false
-    else isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
